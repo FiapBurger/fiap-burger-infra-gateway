314a9decf3ed3dbb4764bed85ae7c4b0aebad5e4
Fiapburger01 <fiapburger.tech@gmail.com>
2024-07-20 16:52:01 -0300

    feat: ajuste workflow 13

--- src/main.tf
+++ src/main.tf
@@ -8,24 +8,18 @@ module "api_gateway" {
 
   apiGatewayName = var.apiGatewayName
   vpcId          = var.vpcId
-  subnetIds      = var.subnetIds
-  targetArns = [
-    module.produtos.target_group_arn
-  ]
-  subnetA              = var.subnetA
-  subnetB              = var.subnetB
-  subnetC              = var.subnetA
+  subnetIds      = [var.subnetA, var.subnetB, var.subnetC] # Agrupando as subnets aqui
+  targetArns     = [module.produtos.target_group_arn]
+  subnetA        = var.subnetA
+  subnetB        = var.subnetB
+  subnetC        = var.subnetC
   apiGtwRootResourceId = module.api_gateway.api_gateway_root_resource_id
 }
 
 module "produtos" {
   source = "./modules/api-gateway/produtos"
   vpcId  = var.vpcId
-  subnetIds = [
-    var.subnetA,
-    var.subnetB,
-    var.subnetC
-  ]
+  subnetIds = [var.subnetA, var.subnetB, var.subnetC] # Agrupando as subnets aqui
   apiGtwId             = module.api_gateway.api_gateway_id
   securityGroupId      = module.api_gateway.security_group_id
   vpcLinkId            = module.api_gateway.vpc_link_id
--- src/vars.tf
+++ src/vars.tf
@@ -1,35 +1,30 @@
 variable "apiGatewayName" {
+  description = "Name of the API Gateway"
   type        = string
-  default     = "fiap-gtw-teste"
-  description = "Nome do API Gateway"
 }
 
 variable "vpcId" {
+  description = "VPC ID"
   type        = string
-  default     = "vpc-008220555f42a21df"
-  description = "ID da VPC"
 }
 
 variable "subnetA" {
+  description = "ID of subnet A"
   type        = string
-  default     = "subnet-02332486181d5f757"
-  description = "ID da Subnet A"
 }
 
 variable "subnetB" {
+  description = "ID of subnet B"
   type        = string
-  default     = "subnet-065b86e8bc1297d97"
-  description = "ID da Subnet B"
 }
 
 variable "subnetC" {
+  description = "ID of subnet C"
   type        = string
-  default     = "subnet-060b3fa08edb4c3f7"
-  description = "ID da Subnet C"
 }
 
 variable "subnetIds" {
+  description = "List of subnet IDs"
   type        = list(string)
-  description = "Lista de IDs de subnets"
-  default     = [var.subnetA, var.subnetB, var.subnetC]
+  default     = [] # Defina um valor padrão vazio
 }
