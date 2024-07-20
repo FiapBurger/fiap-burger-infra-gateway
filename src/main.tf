79eca8d8b1213801b03979128ddfb7d3bfcb54e5
Fiapburger01 <fiapburger.tech@gmail.com>
2024-07-20 16:47:58 -0300

    feat: ajuste workflow 12

--- src/main.tf
+++ src/main.tf
@@ -9,25 +9,25 @@ module "api_gateway" {
   apiGatewayName = var.apiGatewayName
   vpcId          = var.vpcId
   subnetIds      = var.subnetIds
-  targetArns     = [
+  targetArns = [
     module.produtos.target_group_arn
   ]
-  subnetA        = var.subnetA
-  subnetB        = var.subnetB
-  subnetC        = var.subnetA
+  subnetA              = var.subnetA
+  subnetB              = var.subnetB
+  subnetC              = var.subnetA
   apiGtwRootResourceId = module.api_gateway.api_gateway_root_resource_id
 }
 
 module "produtos" {
-  source            = "./modules/api-gateway/produtos"
-  vpcId             = var.vpcId
-  subnetIds         = [
+  source = "./modules/api-gateway/produtos"
+  vpcId  = var.vpcId
+  subnetIds = [
     var.subnetA,
     var.subnetB,
     var.subnetC
   ]
-  apiGtwId          = module.api_gateway.api_gateway_id
-  securityGroupId   = module.api_gateway.security_group_id
-  vpcLinkId         = module.api_gateway.vpc_link_id
+  apiGtwId             = module.api_gateway.api_gateway_id
+  securityGroupId      = module.api_gateway.security_group_id
+  vpcLinkId            = module.api_gateway.vpc_link_id
   apiGtwRootResourceId = module.api_gateway.api_gateway_root_resource_id
 }
