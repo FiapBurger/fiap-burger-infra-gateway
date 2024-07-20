1e3054b34b603373fcd065935bea1f7f82b6aa94
Fiapburger01 <fiapburger.tech@gmail.com>
2024-07-20 16:52:52 -0300

    feat: ajuste workflow 14

--- src/main.tf
+++ src/main.tf
@@ -6,20 +6,20 @@ provider "aws" {
 module "api_gateway" {
   source = "./modules/api-gateway"
 
-  apiGatewayName = var.apiGatewayName
-  vpcId          = var.vpcId
-  subnetIds      = [var.subnetA, var.subnetB, var.subnetC] # Agrupando as subnets aqui
-  targetArns     = [module.produtos.target_group_arn]
-  subnetA        = var.subnetA
-  subnetB        = var.subnetB
-  subnetC        = var.subnetC
+  apiGatewayName       = var.apiGatewayName
+  vpcId                = var.vpcId
+  subnetIds            = [var.subnetA, var.subnetB, var.subnetC] # Agrupando as subnets aqui
+  targetArns           = [module.produtos.target_group_arn]
+  subnetA              = var.subnetA
+  subnetB              = var.subnetB
+  subnetC              = var.subnetC
   apiGtwRootResourceId = module.api_gateway.api_gateway_root_resource_id
 }
 
 module "produtos" {
-  source = "./modules/api-gateway/produtos"
-  vpcId  = var.vpcId
-  subnetIds = [var.subnetA, var.subnetB, var.subnetC] # Agrupando as subnets aqui
+  source               = "./modules/api-gateway/produtos"
+  vpcId                = var.vpcId
+  subnetIds            = [var.subnetA, var.subnetB, var.subnetC] # Agrupando as subnets aqui
   apiGtwId             = module.api_gateway.api_gateway_id
   securityGroupId      = module.api_gateway.security_group_id
   vpcLinkId            = module.api_gateway.vpc_link_id
