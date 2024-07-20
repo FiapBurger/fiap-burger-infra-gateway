59fe14dc518952adb9a0182856321fa7756776a9
Fiapburger01 <fiapburger.tech@gmail.com>
2024-07-20 17:51:56 -0300

    feat: ajuste workflow 21

--- src/modules/api-gateway/main.tf
+++ src/modules/api-gateway/main.tf
@@ -1,10 +1,9 @@
 module "produtos" {
-  source = "./produtos"
-
-  vpcId               = var.vpcId
-  subnetIds           = var.subnetIds
-  securityGroupId     = aws_security_group.api_gateway_sg.id
-  apiGtwId            = aws_api_gateway_rest_api.fiap_burger_api.id
-  vpcLinkId           = aws_api_gateway_vpc_link.vpc_link.id
+  source               = "./produtos"
+  vpcId                = var.vpcId
+  subnetIds            = var.subnetIds
+  securityGroupId      = aws_security_group.api_gateway_sg[0].id
+  apiGtwId             = aws_api_gateway_rest_api.fiap_burger_api.id
+  vpcLinkId            = aws_api_gateway_vpc_link.vpc_link.id
   apiGtwRootResourceId = var.apiGtwRootResourceId
 }
